551a1775293d15ac7e54cd63b9a9ef1768270e10
jamesstaub <james.staub@gmail.com>
2015-07-28 19:45:52 -0400

    removed tone

--- js/app.js
+++ js/app.js
@@ -104,11 +104,3 @@ function assumedName(){
 }
 
 
-// AUDIO
-var player = new Tone.Player("./path/to/sample.mp3").toMaster();
-Tone.Buffer.onload = function(){
-  player.start();
-}
-
-
-var Mongo_URI = process.env. || 'localhost....'
