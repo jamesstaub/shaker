e9009ed863c766cb0c2254f16b0d9766e6a27fdb
jamesstaub <james.staub@gmail.com>
2015-07-29 11:55:42 -0400

    refactoring gyro polling and auth

--- js/app.js
+++ js/app.js
@@ -2,17 +2,70 @@ var shaker = new Firebase("https://shakalaka.firebaseio.com/");
 var amOnline = new Firebase("https://shakalaka.firebaseio.com/.info/connected");
 var colorChannels = shaker.child("colorChannels");
 var nickname;
-var gyroData = {};
+
+
+
 
 $(window).load(function() {
   // listen for device gyroscope
-  pollGyroscope(30);
+  shaker.authAnonymously(function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+      console.log("id: "+ authData.uid);
+      var sessionRef;
+      var userid = authData.uid
+      var userRef = new Firebase("https://shakalaka.firebaseio.com/presence" + userid);
 
+      amOnline.on('value', function(snapshot) {
+        if (snapshot.val()) {
+          sessionRef = userRef.push();
+          sessionRef.child('ended').onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
+          sessionRef.child('began').set(Firebase.ServerValue.TIMESTAMP);
+          sessionRef.child('nickname').set(nickname || assumedName());
+        }
+      });
+
+      // if succesfully authorized as anonymous, then create nickname field
+      $("#nickname").keypress(function(e) {
+        if(e.which == 13) {
+          nickname = $(this).val();
+          sessionRef.update({nickname: nickname});
+        }
+      });
+
+      pollGyroscope(30, sessionRef);
+
+    }
+  });
 });
 
 
-function pollGyroscope(rate) {
+// update list of logged in users
+shaker.on("value", function(snapshot) {
+  if (snapshot.val()) {
+    var currentUsers = [];
+    for(session in snapshot.val()){
+      for(user in snapshot.val()[session]){
+        // if this session has a nickname propery, and it's not ended (ie. its a current session)
+        if(snapshot.val()[session][user].nickname && !snapshot.val()[session][user].ended){
+          currentUsers.push(snapshot.val()[session][user].nickname);
+        }
+      }
+    }
+    $("#sessions").html(
+      currentUsers.map(function(e){
+        return "<li>"+e+"</li>";
+      })
+    )
+  }
+});
+
+
+function pollGyroscope(rate, sessionRef) {
   // set gyroscope update frequency
+  var gyroData = {};
   gyro.frequency = rate;
   gyro.startTracking(function(o) {
     // get the standard deviation of each gyroscope metric
@@ -24,6 +77,15 @@ function pollGyroscope(rate) {
     alpha = scale(alpha, -10, 10, 0, 256);
     beta = scale(beta, -10, 10, 0, 256);
     gamma = scale(gamma, -10, 10, 0, 256);
+
+    // set current user's gyro data
+    sessionRef.child('gyroData').set({
+      alpha : alpha,
+      beta : beta,
+      gamma : gamma
+    })
+
+
 
     // send values to firebase
     colorChannels.set({
@@ -39,68 +101,11 @@ function pollGyroscope(rate) {
     }, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
-
-    // $("#alpha").html();
-    // $("#beta").html();
-    // $("#gamma").html();
   });
+
+  return gyroData;
 }
 
-shaker.authAnonymously(function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-    console.log("id: "+ authData.uid);
-    var sessionRef;
-    var userid = authData.uid
-    var userRef = new Firebase("https://shakalaka.firebaseio.com/presence" + userid);
-
-    amOnline.on('value', function(snapshot) {
-      if (snapshot.val()) {
-        sessionRef = userRef.push();
-        sessionRef.child('ended').onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
-        sessionRef.child('began').set(Firebase.ServerValue.TIMESTAMP);
-        sessionRef.child('nickname').set(nickname || assumedName());
-      }
-    });
-
-    // if succesfully authorized as anonymous, then create nickname field
-    $("#nickname").keypress(function(e) {
-        if(e.which == 13) {
-          nickname = $(this).val();
-          sessionRef.update({nickname: nickname});
-        }
-      });
-
-
-    // set current user's gyro data
-    sessionRef.child('gyro').set({
-
-    })
-
-  }
-});
-
-// update list of logged in users
-shaker.on("value", function(snapshot) {
-  if (snapshot.val()) {
-    var currentUsers = [];
-    for(session in snapshot.val()){
-        for(user in snapshot.val()[session]){
-          // if this session has a nickname propery, and it's not ended (ie. its a current session)
-          if(snapshot.val()[session][user].nickname && !snapshot.val()[session][user].ended){
-            currentUsers.push(snapshot.val()[session][user].nickname);
-          }
-        }
-    }
-    $("#sessions").html(
-      currentUsers.map(function(e){
-        return "<li>"+e+"</li>";
-      })
-    )
-  }
-});
 
 
 function assumedName(){
